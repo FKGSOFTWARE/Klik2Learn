6cd60961122532eb6d0d4322f812967b7a9a6a02
FKGSOFTWARE <89319837+FKGSOFTWARE@users.noreply.github.com>
2022-05-13 13:43:53 +0100

    removed junk

--- src/components/NumberBtn.js
+++ src/components/NumberBtn.js
@@ -1,12 +1,10 @@
 import { useState } from "react"
 import Modal from "./Modal";
 import Backdrop from "./Backdrop";
-import Button from "./Button";
 
 export default NumberBtn;
 
 function NumberBtn(props) {
-    // const [ btnNotClicked, btnClicked] = useState (false);
     const [ modalIsOpen, setModalIsOpen ] = useState(false);
 
     function modalHandler() {
@@ -15,23 +13,6 @@ function NumberBtn(props) {
     function closeModalHandler () {
         setModalIsOpen(false)
     }
-    // function btnClickedHandler() {
-    //     btnClicked(true)
-    // }
-    // function btnNotClickedHandler () {
-    //     btnClicked(false)
-    // }
-
-//     return (
-//         <div>
-//             <div classsName = "actions">
-//                 <button className = "numberBtn" onClick={btnClickedHandler}>{props.text}</button>
-//             </div>
-//             { btnNotClicked && <Button onClick = {btnNotClickedHandler}/> }
-//             {/* { modalIsOpen && <Backdrop  onClick = {btnNotClickedHandler}/> } */}
-//         </div>
-//     )
-// }
 
     return (
         <div>
